61bf56bae2377f85f2f7fb8c69588a3eefb4946f
vyshnavitallamw <166794340+vyshnavitallamw@users.noreply.github.com>
2024-04-26 12:20:51 +0530

    Update JenkinsLab.java

--- JenkinsLab.java
+++ JenkinsLab.java
@@ -1,5 +1,5 @@
 public class JenkinsLab{
     public static void main(String[] args){
-        System.out.println(x:"Author:Vyshnavi-Github Jenkins Build Demo")
+        System.out.println(x:"Author:Vyshnavi-Github Jenkins Build Demo");
     }
 }
